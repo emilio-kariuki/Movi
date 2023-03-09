4822c49cebf37d0dd3ebbf4ffda94645369e1a42
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-03-09 16:04:28 +0300

    FIX: removed the pagination

--- lib/UI/Home/ViewAll/TopRatedMovies.dart
+++ lib/UI/Home/ViewAll/TopRatedMovies.dart
@@ -34,50 +34,50 @@ class _ViewAllTopRatedState extends State<ViewAllTopRated> {
                     fontWeight: FontWeight.w500)),
           ),
           backgroundColor: Colors.black,
-          bottomNavigationBar: Pagination(
-            numOfPages: 100,
-            selectedPage: _current,
-            pagesVisible: 3,
-            onPageChanged: (page) {
-              setState(() {
-                _current = page;
-                BlocProvider.of<HomeBloc>(context)
-                    .add(GetTrending(page: _current));
-              });
-            },
-            nextIcon: const Icon(
-              Icons.arrow_forward_ios,
-              color: Colors.blue,
-              size: 14,
-            ),
-            previousIcon: const Icon(
-              Icons.arrow_back_ios,
-              color: Colors.blue,
-              size: 14,
-            ),
-            activeTextStyle: const TextStyle(
-              color: Colors.white,
-              fontSize: 14,
-              fontWeight: FontWeight.w700,
-            ),
-            activeBtnStyle: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.blue),
-              shape: MaterialStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(38),
-                ),
-              ),
-            ),
-            inactiveBtnStyle: ButtonStyle(
-              shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(38),
-              )),
-            ),
-            inactiveTextStyle: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
+          // bottomNavigationBar: Pagination(
+          //   numOfPages: 100,
+          //   selectedPage: _current,
+          //   pagesVisible: 3,
+          //   onPageChanged: (page) {
+          //     setState(() {
+          //       _current = page;
+          //       BlocProvider.of<HomeBloc>(context)
+          //           .add(GetTrending(page: _current));
+          //     });
+          //   },
+          //   nextIcon: const Icon(
+          //     Icons.arrow_forward_ios,
+          //     color: Colors.blue,
+          //     size: 14,
+          //   ),
+          //   previousIcon: const Icon(
+          //     Icons.arrow_back_ios,
+          //     color: Colors.blue,
+          //     size: 14,
+          //   ),
+          //   activeTextStyle: const TextStyle(
+          //     color: Colors.white,
+          //     fontSize: 14,
+          //     fontWeight: FontWeight.w700,
+          //   ),
+          //   activeBtnStyle: ButtonStyle(
+          //     backgroundColor: MaterialStateProperty.all(Colors.blue),
+          //     shape: MaterialStateProperty.all(
+          //       RoundedRectangleBorder(
+          //         borderRadius: BorderRadius.circular(38),
+          //       ),
+          //     ),
+          //   ),
+          //   inactiveBtnStyle: ButtonStyle(
+          //     shape: MaterialStateProperty.all(RoundedRectangleBorder(
+          //       borderRadius: BorderRadius.circular(38),
+          //     )),
+          //   ),
+          //   inactiveTextStyle: const TextStyle(
+          //     fontSize: 14,
+          //     fontWeight: FontWeight.w700,
+          //   ),
+          // ),
           body: SafeArea(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 5),
